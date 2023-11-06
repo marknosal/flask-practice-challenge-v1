f9893ee06d95c90aba4470313d7742c9d58766f5
Mark Nosal <mark.c.nosal@gmail.com>
2023-11-05 20:32:56 -0600

    finish challenge

--- server/app.py
+++ server/app.py
@@ -1,9 +1,72 @@
 from config import app, api
+from flask import request, jsonify
 from models import Post, Comment
 from flask_restful import Resource
+from sqlalchemy import func
 
 # create routes here:
+class Posts(Resource):
+  def get(self):
+    posts = [p.to_dict() for p in Post.query.all()]
 
+    return posts, 200 
+
+class SortedPosts(Resource):
+  def get(self):
+    posts = [p.to_dict() for p in Post.query.all()]
+    sorted_posts = sorted(posts, key=lambda p: p['title'])
+    
+    return sorted_posts, 200
+
+class PostsByAuthor(Resource):
+  def get(self, author):
+    posts = Post.query.filter(func.lower(Post.author)==author.lower()).all()
+    posts_dict = [p.to_dict() for p in posts]
+    
+    return posts_dict, 200
+
+class SearchPosts(Resource):
+  def get(self, title):
+    posts = Post.query.filter(Post.title.ilike(f"%{title}%")).all()
+    # posts = Post.query.filter(func.lower(Post.title).contains(title.lower())).all()
+    posts_dict = [p.to_dict() for p in posts]
+
+    return posts_dict, 200
+  
+class PostsOrderedByComments(Resource):
+  def get(self):
+    posts = [p.to_dict() for p in Post.query.all()]
+    sorted_posts = sorted(posts, key=lambda p: len(p['comments']), reverse=True)
+    
+    return sorted_posts, 200
+  
+class MostPopularCommenter(Resource):
+  def get(self):
+    comment_counts = {}
+    most_popular_commenter = None
+    most_comments = 0
+    for post in Post.query.all():
+      for comment in post.comments:
+        commenter = comment.commenter
+        if commenter in comment_counts:
+          comment_counts[commenter] += 1
+        else:
+          comment_counts[commenter] = 1
+        if comment_counts[commenter] > most_comments:
+          most_comments = comment_counts[commenter]
+          most_popular_commenter = commenter
+
+    return { 'commenter': most_popular_commenter, 'count': most_comments }, 200
+
+
+    
+
+api.add_resource(Posts, '/api/posts')
+api.add_resource(SortedPosts, '/api/sorted_posts')
+api.add_resource(PostsByAuthor, '/api/posts_by_author/<string:author>')
+api.add_resource(SearchPosts, '/api/search_posts/<string:title>')
+api.add_resource(PostsOrderedByComments, '/api/posts_ordered_by_comments')
+api.add_resource(MostPopularCommenter, '/api/most_popular_commenter')
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -16,17 +16,20 @@ with app.app_context():
   post_1_comment_1 = Comment(commenter="Sam", post_id=post_1.id, content="Gross!")
   post_1_comment_2 = Comment(commenter="Sara", post_id=post_1.id, content="So sorry to hear! That happened to me too!")
   post_1_comment_3 = Comment(commenter="Frank", post_id=post_1.id, content="Maybe it was the frog next to that frog??? Try again??")
+  post_1_comment_4 = Comment(commenter='Mark', post_id=post_1.id, content='test post')
 
-  db.session.add_all([post_1_comment_1, post_1_comment_2, post_1_comment_3])
+  db.session.add_all([post_1_comment_1, post_1_comment_2, post_1_comment_3, post_1_comment_4])
   db.session.commit()
 
   post_2_comment_1 = Comment(commenter="Sam", post_id=post_2.id, content="I step on legos all the time!")
   post_2_comment_2 = Comment(commenter="Frank", post_id=post_2.id, content="My son leaves legos all over the floor. I step on them on purpose because if I'm going down, it's going to be on my terms!")
+  post_2_comment_3 = Comment(commenter='Mark', post_id=post_1.id, content='test post')
 
-  db.session.add_all([post_2_comment_1, post_2_comment_2])
+  db.session.add_all([post_2_comment_1, post_2_comment_2, post_2_comment_3])
   db.session.commit()
 
   post_3_comment_1 = Comment(commenter="Sara", post_id=post_3.id, content="Cute Dog!")
+  post_3_comment_2 = Comment(commenter='Mark', post_id=post_1.id, content='test post')
 
-  db.session.add(post_3_comment_1)
+  db.session.add_all([post_3_comment_1, post_3_comment_2])
   db.session.commit()
